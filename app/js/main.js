79e7c7fc1111e7c63fc164fb60acdde6f6ff4d74
Артём <artsportsq@mail.ru>
2024-11-21 10:38:52 +0300

    added contacts-page

--- app/js/main.js
+++ app/js/main.js
@@ -124,4 +124,17 @@ $(function () {
 
 	const deadline = $('.promo__clock').attr('data-time')
 	initializeClock('promo__clock', deadline)
+
+	let map
+
+	async function initMap() {
+		const { Map } = await google.maps.importLibrary('maps')
+
+		map = new Map(document.getElementById('map'), {
+			center: { lat: -34.397, lng: 150.644 },
+			zoom: 8,
+		})
+	}
+
+	initMap()
 })
